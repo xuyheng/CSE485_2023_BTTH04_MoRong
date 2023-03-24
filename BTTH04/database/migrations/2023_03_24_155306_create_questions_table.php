101daf5898833c72c63a6d25e0468827c7239734
thuyhang <1951060690@e.tlu.edu.vn>
2023-03-24 23:05:30 +0700

    first commit

---
+++ BTTH04/database/migrations/2023_03_24_155306_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('exam_id');
+            $table->text('content');
+            $table->enum('type', ['single_choice', 'multiple_choice', 'true_false']);
+            $table->json('options')->nullable();
+            $table->timestamps();
+            
+            $table->foreign('exam_id')
+                ->references('id')->on('exams')
+                ->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
